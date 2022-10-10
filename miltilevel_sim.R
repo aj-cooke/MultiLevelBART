41c998b9d7690432306d0345132e7cdf0582cec1
aj-cooke <62916946+aj-cooke@users.noreply.github.com>
2022-10-10 14:27:56 -0400

    Added "superintendent" selection

--- miltilevel_sim.R
+++ miltilevel_sim.R
@@ -65,6 +65,82 @@ linear_treatment_ind <- function(data){
   return(data)
 }
 
+# can enhance by recursively splitting.
+# This approach may require making every variable share a positive relationship with treatment so splits can be made intuitive
+discrete_treatment_group <- function(data){
+  grouped <- data %>%
+    group_by(group) %>%
+    summarise(across(
+      .cols = where(is.numeric), 
+      .fns = list(Mean = mean), na.rm = TRUE, 
+      .names = "{col}_{fn}"
+    ))
+  grouped <- data.frame(grouped)
+  
+  selection_col <- sample.int(ncol(grouped),1)
+  cutoff <- quantile(grouped[,selection_col], probs = runif(1,0.25,0.75))
+  grouped$z <- if_else(grouped[,selection_col] >= cutoff, 1, 0)
+  data <- merge(data, subset(grouped, select = c("group", "z")), by = "group", all.x = T, sort = F)
+  return(data)
+}
+
+# can add saving rules, sometimes errors if splits go haywire but usually ok
+discrete_rec <- function(data, depth = 0, max_depth = 2, treat = 0){
+  print(data)
+  print(depth)
+  if(depth < max_depth){
+    selection_col <- sample.int(ncol(data),1)
+    data[,"selection_col"] <- data[,selection_col]
+    cutoff <- quantile(data$selection_col, probs = runif(1,0.25,0.75))
+    right_df <- data %>% filter(selection_col < cutoff) %>% dplyr::select(-selection_col)
+    left_df <- data %>% filter(selection_col >= cutoff) %>% dplyr::select(-selection_col)
+    return(rbind(discrete_rec(right_df, depth + 1, max_depth), 
+                 discrete_rec(left_df, depth + 1, max_depth, treat = 1)))
+  }
+  else{
+    data$z <- treat
+    return(data)
+  }
+}
+
+discrete_rec <- function(data, depth = 0, max_depth = 2, treat = 0){
+  if(depth < max_depth){
+    selection_col <- sample.int(ncol(data),1)
+    data[,"selection_col"] <- data[,selection_col]
+    cutoff <- quantile(data$selection_col, probs = runif(1,0.25,0.75))
+    right_df <- data %>% filter(selection_col < cutoff) %>% dplyr::select(-selection_col)
+    left_df <- data %>% filter(selection_col >= cutoff) %>% dplyr::select(-selection_col)
+    if(nrow(right_df) == 0){
+      return(discrete_rec(left_df, depth + 1, max_depth, treat = 1))
+    }
+    else if(nrow(left_df) == 0){
+      return(discrete_rec(right_df, depth + 1, max_depth))
+    }
+    else{
+      return(rbind(discrete_rec(right_df, depth + 1, max_depth), 
+                   discrete_rec(left_df, depth + 1, max_depth, treat = 1)))
+    }
+  }
+  else{
+    data$z <- treat
+    return(data)
+  }
+}
+
+discrete_rec_group <- function(data){
+  grouped <- data %>%
+    group_by(group) %>%
+    summarise(across(
+      .cols = where(is.numeric), 
+      .fns = list(Mean = mean), na.rm = TRUE, 
+      .names = "{col}_{fn}"
+    ))
+  grouped <- data.frame(grouped)
+  grouped <- discrete_rec(grouped)
+  data <- merge(data, subset(grouped, select = c("group", "z")), by = "group", all.x = T, sort = F)
+  return(data)
+}
+
 gen_multilevel <- function(n, n_groups, k_ind, k_group, group_p = "none", assignment = "group"){
   data <- data.frame(generate_mvn(n, k_ind)) # first just ind level
   
@@ -107,8 +183,40 @@ gen_multilevel <- function(n, n_groups, k_ind, k_group, group_p = "none", assign
   # assign treatment
   if(assignment == "group"){data <- linear_treatment_group(data)}
   else if(assignment %in% c("ind", "individual")){data <- linear_treatment_ind(data)}
-  
+  else if(assignment %in% c("discrete")){data <- discrete_treatment_group(data)}
+  else if(assignment %in% c("rec_ind")){data <- discrete_rec(data)}
+  else if(assignment %in% c("rec_group")){data <- discrete_rec_group(data)}
   return(data)
 }
 
-data = gen_multilevel(10000, 6, 7,3, assignment = "ind")
+# TESTING
+
+data1 = gen_multilevel(10000, 6, 7,3)
+data2 = gen_multilevel(10000, 6, 7,3, assignment = "ind")
+data3 = gen_multilevel(10000, 6, 7,3, assignment = "discrete")
+data4 = gen_multilevel(10000, 6, 7,3, assignment = "rec_ind")
+data5 = gen_multilevel(10000, 6, 7,3, assignment = "rec_group")
+
+
+num_ind_invalid <- 0
+num_group_invalid <- 0
+
+num_ind_broke <- 0
+num_group_broke <- 0
+
+for(i in 1:100){
+  data4 = gen_multilevel(10000, 6, 7,3, assignment = "rec_ind")
+  print('ind')
+  data5 = gen_multilevel(10000, 6, 7,3, assignment = "rec_group")
+  print('group')
+  if(nrow(data4 %>% filter(z==1)) == 0){num_ind_invalid = num_ind_invalid+1}
+  if(nrow(data4 %>% filter(z==0)) == 0){num_ind_invalid = num_ind_invalid+1}
+  if(nrow(data5 %>% filter(z==1)) == 0){num_group_invalid = num_group_invalid+1}
+  if(nrow(data5 %>% filter(z==0)) == 0){num_group_invalid = num_group_invalid+1}
+  
+  if(nrow(data4) != 10000){num_ind_broke = num_ind_broke + 1}
+  if(nrow(data5) != 10000){num_group_broke = num_group_broke + 1}
+  print(i)
+}
+
+
